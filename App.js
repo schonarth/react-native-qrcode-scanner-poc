962353b9f1094fde778b5d64309517ce96897e92
Gustavo Schonarth <gustavo.schonarth@meta.com.br>
2022-09-09 11:52:39 -0300

    Implement capacity to read QR and barcodes, switch cameras, and turn them on and off.
    If a URL is detected, open it in the default browser.

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import Scanner from './Scanner';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Switch } from 'react-native';
 
 export default function App() {
+  const emptyRead = 'Your result will appear here';
+  const [scanData, setScanData] = React.useState(undefined);
+  const [scannerOn, setScannerOn] = React.useState(true);
+
+  const handleScan = (data) => {
+    console.log(data);
+    setScanData(data);
+  };
+
+  const handlePress = () => {
+    setScannerOn(!scannerOn);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <Scanner />
-    </View>
+      {scannerOn && (
+        <Scanner
+          style={{ flex: 1 }}
+          onScan={handleScan}
+          reactivateTimeout={1000}
+        />
+      )}
+      <View style={styles.bottom}>
+        <View style={styles.readOutput}>
+          <Text style={!!scanData && styles.readOutField}>
+            {scanData || emptyRead}
+          </Text>
+        </View>
+        <View style={styles.switch}>
+          <Text>Camera on</Text>
+          <Switch value={scannerOn} onValueChange={handlePress} />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -19,5 +48,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bottom: {
+    flex: 0.1,
+    flexDirection: 'row',
+  },
+  readOutput: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  readOutField: {
+    backgroundColor: '#eee',
+    padding: 10,
+    borderRadius: 10,
+  },
+  switch: {
+    flex: 0.3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
   },
 });
--- Scanner.jsx
+++ Scanner.jsx
@@ -7,31 +7,74 @@ import {
   Text,
   TouchableOpacity,
   Linking,
+  Alert,
 } from 'react-native';
-import { RNCamera } from 'react-native-camera';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 
 class Scanner extends Component {
+  constructor() {
+    super();
+    this.cameraIcons = {
+      front: '💻',
+      back: '🎮',
+    };
+
+    this.state = {
+      currentCamera:
+        this.props?.camera?.toLowerCase() === 'front' ? 'front' : 'back',
+    };
+  }
+
+  switchCameras() {
+    this.setState({
+      currentCamera: this.state.currentCamera === 'front' ? 'back' : 'front',
+    });
+    console.log('Switching cameras:', this.state.currentCamera);
+  }
+
   onSuccess(e) {
-    Linking.openURL(e.data).catch((err) =>
-      console.error('An error occured', err)
-    );
+    if (e.data?.startsWith('http')) {
+      Alert.alert('URL found', `Open ${e.data} in browser?`, [
+        {
+          text: 'Yes',
+          onPress: () =>
+            Linking.openURL(e.data).catch((err) =>
+              console.error('An error occured', err)
+            ),
+        },
+        {
+          text: 'No',
+          onPress: () => console.log('No pressed'),
+          style: 'cancel',
+        },
+      ]);
+    }
+    if (this.props.onScan) this.props.onScan(e.data);
   }
 
   render() {
     return (
       <QRCodeScanner
         onRead={this.onSuccess.bind(this)}
-        topContent={
-          <Text style={styles.centerText}>
-            Go to{' '}
-            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-            your computer and scan the QR code.
-          </Text>
-        }
+        showMarker
+        reactivate
+        reactivateTimeout={this.props.reactivateTimeout || 5000}
+        cameraType={this.state.currentCamera}
+        // topContent={
+        //   <Text style={styles.centerText}>
+        //     Go to{' '}
+        //     <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+        //     your computer and scan the QR code.
+        //   </Text>
+        // }
         bottomContent={
-          <TouchableOpacity style={styles.buttonTouchable}>
-            <Text style={styles.buttonText}>OK. Got it!</Text>
+          <TouchableOpacity
+            style={styles.buttonTouchable}
+            onPress={this.switchCameras.bind(this)}
+          >
+            <Text style={styles.buttonText}>
+              {this.cameraIcons[this.state.currentCamera]} Switch cameras
+            </Text>
           </TouchableOpacity>
         }
       />
@@ -56,6 +99,9 @@ const styles = StyleSheet.create({
   },
   buttonTouchable: {
     padding: 16,
+    backgroundColor: '#fff9',
+    borderRadius: 10,
+    marginBottom: 40,
   },
 });
 
